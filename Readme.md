131367bac0a9fbad9596ef6ffd6f50a20fc341a6
Anne Gaitate <anne.nkone@gmail.com>
2023-08-03 10:12:42 +0200

    Modify files

--- Readme.md
+++ Readme.md
@@ -1,20 +1,25 @@
 ## Project Name: Horoscope message generator
 
-### Description
+### Description:
 
-* This program was build for entertainment and fun.Some individuals believe that horoscope messages provide insights into their personality traits, strengths, and challenges.
+* This program was build for fun and entertainment.
+
+* It generates random horoscopic messages everytime user runs the program.
+
+* Some people believe that horoscope messages provide insights into their personality traits, strengths, and challenges.
 
 * I'm open for suggestions and modifications.
 
 
-### Technologies
+### Technologies used:
 
 * Only JavaScript used for this program, this is because of its simplicity and easy to understand methods.
 
+* Node.js to run and debug code
 
-### How it works
+### How it works:
 
-* This program generates random horoscopic messages everytime a user runs the program.
+* The user should run the program as many times as they wish, everytime the program runs a random message is logged to the console.
 
 
 ![Horoscope Wheel](https://cdn.pixabay.com/animation/2023/01/12/18/10/18-10-43-580_512.gif)
--- myluckyMessage.js
+++ myluckyMessage.js
@@ -2,32 +2,34 @@
 
  randomMessage = () => {//This arrow function takes no parameters
 
+
     let randomNumber = Math.floor(Math.random() * 6); //Generate random whole numbers between 0 and 5 then stores them in a declared randomNumber variable
+
 
     switch (randomNumber) { //Switch statement assign messages to random numbers generated
 
         case 0:
-            return 'Your lucky planet right now is: Mars \nMessage for you: Dont try to solve everything, Let go and let God!';
+            return 'Your lucky planet now : Mars \nMessage : Do not try to resolve everything, Let go and let God!';
             break;
 
         case 1:
-            return 'Your lucky planet right now is: Jupiter \nMessage for you: You are highly capable never doubt yourself again.';
+            return 'Your lucky planet now : Jupiter \nMessage : You are highly capable never doubt yourself again.';
             break;
 
         case 2:
-            return 'Your lucky planet right now is: Venus \nMessage for you: Do not be anxious about the future or the past.';
+            return 'Your lucky planet now : Venus \nMessage : Do not be anxious about the future or the past.';
             break;
 
         case 3:
-            return 'Your lucky planet right now is: Earth \nMessage for you: Be patient the universe will deliver.';
+            return 'Your lucky planet now : Earth \nMessage : Be patient the universe will deliver.';
             break;
 
         case 4:
-            return 'Your lucky planet right now is: Saturn \nMessage for you: Stop procrastinating and take action right now!';
+            return 'Your lucky planet now : Saturn \nMessage : Stop procrastinating and take action right now!';
             break;
 
         case 5:
-            return 'Your lucky planet right now is: Uranus \nMessage for you: The future looks bright stop overthinking.';
+            return 'Your lucky planet now : Uranus \nMessage : The future looks bright stop overthinking.';
             break;
 
         default:
